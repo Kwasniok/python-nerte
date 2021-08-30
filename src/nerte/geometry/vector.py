d452252b6bebcdfdfd34e1dc440faea13a4ba71e
Kwasniok <Kwasniok@users.noreply.github.com>
2021-08-30 06:56:31 +0200

    add metric

--- src/nerte/geometry/vector.py
+++ src/nerte/geometry/vector.py
@@ -8,12 +8,12 @@ import numpy as np
 class AbstractVector:
     """
     Represents an abstract vector via its three real coefficients.
-    Note: The basis of the vectorspace must be implicitly given by the contex
-          the vector is used in.
+    Note: The basis of the vector space and its kindness (co- or contra-variant)
+          must be implicitly given by the context the vector is used in.
     """
 
     def __init__(self, v1: float, v2: float, v3: float) -> None:
-        self._v = np.array([v1, v2, v3])
+        self._v = np.array((v1, v2, v3))
 
     def __repr__(self) -> str:
         return "V(" + (",".join(repr(x) for x in self._v)) + ")"
@@ -48,26 +48,95 @@ def _abstract_vector_from_numpy(np_array: np.ndarray) -> AbstractVector:
     return vec
 
 
-class ContraVector(AbstractVector):
+class AbstractMatrix:
     """
-    Represents a contra-variant vector via its three real coefficients.
-    Note: The basis of the vectorspace must be implicitly given by the contex
-          the vector is used in.
-    """
-
-    # pylint: disable=R0903,W0107
-    pass
-
-
-class CoVector(AbstractVector):
-    """
-    Represents a co-variant vector via its three real coefficients.
-    Note: The basis of the vectorspace must be implicitly given by the contex
-          the vector is used in.
+    Represents an abstract matrix via three vectors.
+    Note: The basis of the vector space and the kindness (co- or contra-variant)
+          of each rank must be implicitly given by the context the vector is
+          used in.
     """
 
-    # pylint: disable=R0903,W0107
-    pass
+    def __init__(
+        self, vec0: AbstractVector, vec1: AbstractVector, vec2: AbstractVector
+    ) -> None:
+        self._m = np.array((vec0._v, vec1._v, vec2._v))
+
+    def __repr__(self) -> str:
+        return "M(" + (",".join(repr(x) for x in self._m)) + ")"
+
+    def __add__(self, other: "AbstractMatrix") -> "AbstractMatrix":
+        return _abstract_matrix_from_numpy(self._m + other._m)
+
+    def __neg__(self) -> "AbstractMatrix":
+        return _abstract_matrix_from_numpy(-1 * self._m)
+
+    def __sub__(self, other: "AbstractMatrix") -> "AbstractMatrix":
+        return _abstract_matrix_from_numpy(self._m - other._m)
+
+    def __mul__(self, fac: float) -> "AbstractMatrix":
+        return _abstract_matrix_from_numpy(fac * self._m)
+
+    def __truediv__(self, fac: float) -> "AbstractMatrix":
+        return _abstract_matrix_from_numpy((1 / fac) * self._m)
+
+    def __getitem__(self, i: int) -> AbstractVector:
+        return _abstract_vector_from_numpy(self._m[i])
+
+
+def _abstract_matrix_from_numpy(np_array: np.ndarray) -> AbstractMatrix:
+    """
+    Auxiliar function to wrap an np.array into a matrix.
+    Note: For internal usage only! The input is trusted to be valid and no
+          checks are applied.
+    """
+    mat = AbstractMatrix.__new__(AbstractMatrix)
+    mat._m = np_array
+    return mat
+
+
+class Metric:
+    """
+    Represents a metric as a matrix acting on contravariant representations of
+    vectors and returning covariant representations of them.
+    Note: A metric must be invertible.
+    """
+
+    def __init__(self, matrix: AbstractMatrix) -> None:
+        self._g = matrix
+        try:
+            self._g_inv = inverted(matrix)
+        except ArithmeticError as ex:
+            raise ValueError(
+                "Cannot construct a metric from non-invertible matrix."
+            ) from ex
+
+    def matrix(self) -> AbstractMatrix:
+        """Returns the metric as a matrix."""
+        return self._g
+
+    def inverse_matrix(self) -> AbstractMatrix:
+        """Returns the inverse of the metric as a matrix."""
+        return self._g_inv
+
+
+def covariant(metric: Metric, contra_vec: AbstractVector) -> AbstractVector:
+    """
+    Returns the co-variant vector.
+
+    :param metric: metric of the tangential vector space
+    :param contra_vec: contra-variant vector of the tangential vetor space
+    """
+    return _abstract_vector_from_numpy(np.dot(metric._g._m, contra_vec._v))
+
+
+def contravariant(metric: Metric, co_vec: AbstractVector) -> AbstractVector:
+    """
+    Returns the contra-variant vector.
+
+    :param inv_metric: inverse of the metric of the tangential vector space
+    :param contra_vec: co-variant vector of the tangential vetor space
+    """
+    return _abstract_vector_from_numpy(np.dot(metric._g_inv._m, co_vec._v))
 
 
 def dot(vec1: AbstractVector, vec2: AbstractVector) -> float:
@@ -109,3 +178,16 @@ def normalized(vec: AbstractVector) -> AbstractVector:
     return _abstract_vector_from_numpy(vec._v * (dot(vec, vec) ** -0.5))
     # NOTE: DON'T use this:
     # return Vector.__from_numpy((1 / np.linalg.norm(vec._v)) * vec._v)
+
+
+def inverted(mat: AbstractMatrix) -> AbstractMatrix:
+    """
+    Returns the inverse of a matrix.
+    :raises: ArithmeticError
+    """
+    try:
+        mat = _abstract_matrix_from_numpy(np.linalg.inv(mat._m))
+    except np.linalg.LinAlgError as ex:
+        raise ArithmeticError from ex
+    else:
+        return mat
--- src/nerte/geometry/vector_unittest.py
+++ src/nerte/geometry/vector_unittest.py
@@ -7,7 +7,18 @@
 
 import unittest
 import math
-from nerte.geometry.vector import AbstractVector, dot, cross, length, normalized
+from nerte.geometry.vector import (
+    AbstractVector,
+    AbstractMatrix,
+    Metric,
+    covariant,
+    contravariant,
+    dot,
+    cross,
+    length,
+    normalized,
+    inverted,
+)
 
 
 # equivalence of floating point representations with finite precision
@@ -22,18 +33,16 @@ def _vec_equiv(x: AbstractVector, y: AbstractVector) -> bool:
     return _equiv(x[0], y[0]) and _equiv(x[1], y[1]) and _equiv(x[2], y[2])
 
 
-# standart Carthesian basis
-orth_norm_basis = (
-    AbstractVector(1.0, 0.0, 0.0),
-    AbstractVector(0.0, 1.0, 0.0),
-    AbstractVector(0.0, 0.0, 1.0),
-)
-
-# arbitrary factors
-scalar_factors = (0.0, 1.2345, -0.98765)
+# True, iff two matrices component-wise agree up to the (absolute) precision 𝜀
+def _mat_equiv(x: AbstractMatrix, y: AbstractMatrix) -> bool:
+    return (
+        _vec_equiv(x[0], y[0])
+        and _vec_equiv(x[1], y[1])
+        and _vec_equiv(x[2], y[2])
+    )
 
 
-class VectorTest(unittest.TestCase):
+class LinAlgTestCase(unittest.TestCase):
     def assertEquiv(self, x: float, y: float) -> None:
         """
         Asserts the equivalence of two floats.
@@ -49,7 +58,7 @@ class VectorTest(unittest.TestCase):
     def assertVectorEquiv(self, x: AbstractVector, y: AbstractVector) -> None:
         """
         Asserts ths equivalence of two vectors.
-        Note: This replaces assertTrue(x == y) for nerte.Vector.
+        Note: This replaces assertTrue(x == y) for vectors.
         """
         try:
             self.assertTrue(_vec_equiv(x, y))
@@ -58,44 +67,148 @@ class VectorTest(unittest.TestCase):
                 "Vector {} is not equivalent to {}.".format(x, y)
             ) from ae
 
-    def test_item(self) -> None:
+    def assertMatrixEquiv(self, x: AbstractMatrix, y: AbstractMatrix) -> None:
+        """
+        Asserts ths equivalence of two matrices.
+        Note: This replaces assertTrue(x == y) for matrices.
+        """
+        try:
+            self.assertTrue(_mat_equiv(x, y))
+        except AssertionError as ae:
+            raise AssertionError(
+                "Matrix {} is not equivalent to {}.".format(x, y)
+            ) from ae
+
+
+class AbstractVectorTestItem(LinAlgTestCase):
+    def setUp(self) -> None:
+        self.cs = (1.0, 2.0, 3.0)
+
+    def test_vector_item(self) -> None:
         """Tests item related operations."""
-        cs = (1.0, 2.0, 3.0)
-        v = AbstractVector(*cs)
-        for i, c in zip(range(3), cs):
+        v = AbstractVector(*self.cs)
+        for i, c in zip(range(3), self.cs):
             self.assertEquiv(v[i], c)
 
-    def test_math_linear(self) -> None:
+
+class AbstractVectorMathTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        self.v1 = AbstractVector(1.1, 2.2, 3.3)
+        self.v2 = AbstractVector(4.4, 5.5, 6.6)
+        self.v3 = AbstractVector(5.5, 7.7, 9.9)
+        self.v4 = AbstractVector(1.0, 1.0, 1.0)
+        self.v5 = AbstractVector(3.3, 3.3, 3.3)
+
+    def test_vector_linear(self) -> None:
         """Tests linear operations on vectors."""
+
+        self.assertVectorEquiv(self.v1 + self.v2, self.v3)
+        self.assertVectorEquiv(self.v4 * 3.3, self.v5)
+        self.assertVectorEquiv(self.v2 - self.v1, self.v5)
+        self.assertVectorEquiv(self.v5 / 3.3, self.v4)
+
+
+class AbstractMatrixTestItem(LinAlgTestCase):
+    def setUp(self) -> None:
+        self.vs = (
+            AbstractVector(1.0, 2.0, 3.0),
+            AbstractVector(4.0, 5.0, 6.0),
+            AbstractVector(7.0, 8.0, 9.0),
+        )
+
+    def test_matrix_item(self) -> None:
+        """Tests item related operations."""
+        m = AbstractMatrix(*self.vs)
+        for i, v in zip(range(3), self.vs):
+            self.assertVectorEquiv(m[i], v)
+
+
+class AbstractMatrixMathTest(LinAlgTestCase):
+    def setUp(self) -> None:
         v1 = AbstractVector(1.1, 2.2, 3.3)
         v2 = AbstractVector(4.4, 5.5, 6.6)
         v3 = AbstractVector(5.5, 7.7, 9.9)
         v4 = AbstractVector(1.0, 1.0, 1.0)
         v5 = AbstractVector(3.3, 3.3, 3.3)
+        self.m1 = AbstractMatrix(v1, v1, v1)
+        self.m2 = AbstractMatrix(v2, v2, v2)
+        self.m3 = AbstractMatrix(v3, v3, v3)
+        self.m4 = AbstractMatrix(v4, v4, v4)
+        self.m5 = AbstractMatrix(v5, v5, v5)
 
-        self.assertVectorEquiv(v1 + v2, v3)
-        self.assertVectorEquiv(v4 * 3.3, v5)
-        self.assertVectorEquiv(v2 - v1, v5)
-        self.assertVectorEquiv(v5 / 3.3, v4)
+    def test_matrix_linear(self) -> None:
+        """Tests linear operations on matrices."""
 
-    def test_math_length(self) -> None:
+        self.assertMatrixEquiv(self.m1 + self.m2, self.m3)
+        self.assertMatrixEquiv(self.m4 * 3.3, self.m5)
+        self.assertMatrixEquiv(self.m2 - self.m1, self.m5)
+        self.assertMatrixEquiv(self.m5 / 3.3, self.m4)
+
+
+class MetricTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        self.m0 = AbstractMatrix(
+            AbstractVector(0.0, 0.0, 0.0),
+            AbstractVector(0.0, 0.0, 0.0),
+            AbstractVector(0.0, 0.0, 0.0),
+        )
+        self.m = AbstractMatrix(
+            AbstractVector(2.0, 3.0, 5.0),
+            AbstractVector(7.0, 11.0, 13.0),
+            AbstractVector(17.0, 19.0, 23.0),
+        )
+        self.m_inv = inverted(self.m)
+
+    def test_metric_constructor(self) -> None:
+        """Tests metric constructor."""
+        Metric(self.m)
+        with self.assertRaises(ValueError):
+            Metric(self.m0)
+
+    def test_metric_matrix_getters(self) -> None:
+        """Tests metric matrix getters."""
+        g = Metric(self.m)
+        self.assertMatrixEquiv(g.matrix(), self.m)
+        self.assertMatrixEquiv(g.inverse_matrix(), self.m_inv)
+
+
+class AbstractVectorLengthTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        self.v0 = AbstractVector(0.0, 0.0, 0.0)
+        self.v1 = AbstractVector(1.0, 2.0, -3.0)
+
+    def test_vector_length(self) -> None:
         """Tests vector length."""
-        v0 = AbstractVector(0.0, 0.0, 0.0)
-        v1 = AbstractVector(1.0, 2.0, -3.0)
 
-        self.assertEquiv(length(v0), 0.0)
-        self.assertEquiv(length(v1) ** 2, 14.0)
+        self.assertEquiv(length(self.v0), 0.0)
+        self.assertEquiv(length(self.v1) ** 2, 14.0)
 
-    def test_math_normalized(self) -> None:
+
+class AbstractVectorNormalizedTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        self.n = AbstractVector(1.0, 1.0, 1.0) / math.sqrt(3)
+        self.w = AbstractVector(7.0, 7.0, 7.0)
+
+    def test_vector_normalized(self) -> None:
         """Tests vector normalization."""
-        n = AbstractVector(1.0, 1.0, 1.0) / math.sqrt(3)
-        w = AbstractVector(7.0, 7.0, 7.0)
-        self.assertVectorEquiv(normalized(w), n)
+        self.assertVectorEquiv(normalized(self.w), self.n)
+
+
+class DotTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        # standart Carthesian basis
+        self.orth_norm_basis = (
+            AbstractVector(1.0, 0.0, 0.0),
+            AbstractVector(0.0, 1.0, 0.0),
+            AbstractVector(0.0, 0.0, 1.0),
+        )
+        # arbitrary factors
+        self.scalar_factors = (0.0, 1.2345, -0.98765)
 
     def test_math_dot_orthonormality(self) -> None:
         """Tests dot product acting on orthonormal basis."""
-        for v in orth_norm_basis:
-            for w in orth_norm_basis:
+        for v in self.orth_norm_basis:
+            for w in self.orth_norm_basis:
                 if v is w:
                     self.assertEquiv(dot(v, w), 1.0)
                 else:
@@ -103,11 +216,11 @@ class VectorTest(unittest.TestCase):
 
     def test_math_dot_linearity_left(self) -> None:
         """Tests dot product's linearity in the left argument."""
-        for u in orth_norm_basis:
-            for v in orth_norm_basis:
-                for w in orth_norm_basis:
-                    for a in scalar_factors:
-                        for b in scalar_factors:
+        for u in self.orth_norm_basis:
+            for v in self.orth_norm_basis:
+                for w in self.orth_norm_basis:
+                    for a in self.scalar_factors:
+                        for b in self.scalar_factors:
                             self.assertEquiv(
                                 dot((v * a) + (w * b), u),
                                 dot(v, u) * a + dot(w, u) * b,
@@ -115,29 +228,41 @@ class VectorTest(unittest.TestCase):
 
     def test_math_dot_linearity_right(self) -> None:
         """Tests dot product's linearity in the right argument."""
-        for u in orth_norm_basis:
-            for v in orth_norm_basis:
-                for w in orth_norm_basis:
-                    for a in scalar_factors:
-                        for b in scalar_factors:
+        for u in self.orth_norm_basis:
+            for v in self.orth_norm_basis:
+                for w in self.orth_norm_basis:
+                    for a in self.scalar_factors:
+                        for b in self.scalar_factors:
                             self.assertEquiv(
                                 dot(u, (v * a) + (w * b)),
                                 dot(u, v) * a + dot(u, w) * b,
                             )
 
+
+class CrossTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        # standart Carthesian basis
+        self.orth_norm_basis = (
+            AbstractVector(1.0, 0.0, 0.0),
+            AbstractVector(0.0, 1.0, 0.0),
+            AbstractVector(0.0, 0.0, 1.0),
+        )
+        # arbitrary factors
+        self.scalar_factors = (0.0, 1.2345, -0.98765)
+
     def test_math_cross_orthonormality(self) -> None:
         """Tests cross product acting on orthonormal basis."""
-        for v in orth_norm_basis:
-            for w in orth_norm_basis:
+        for v in self.orth_norm_basis:
+            for w in self.orth_norm_basis:
                 self.assertVectorEquiv(cross(v, w), -cross(w, v))
 
     def test_math_cross_linearity_left(self) -> None:
         """Tests cross product's linearity in the left argument."""
-        for u in orth_norm_basis:
-            for v in orth_norm_basis:
-                for w in orth_norm_basis:
-                    for a in scalar_factors:
-                        for b in scalar_factors:
+        for u in self.orth_norm_basis:
+            for v in self.orth_norm_basis:
+                for w in self.orth_norm_basis:
+                    for a in self.scalar_factors:
+                        for b in self.scalar_factors:
                             self.assertVectorEquiv(
                                 cross((v * a) + (w * b), u),
                                 cross(v, u) * a + cross(w, u) * b,
@@ -145,15 +270,104 @@ class VectorTest(unittest.TestCase):
 
     def test_math_cross_linearity_right(self) -> None:
         """Tests cross product's linearity in the right argument."""
-        for u in orth_norm_basis:
-            for v in orth_norm_basis:
-                for w in orth_norm_basis:
-                    for a in scalar_factors:
-                        for b in scalar_factors:
+        for u in self.orth_norm_basis:
+            for v in self.orth_norm_basis:
+                for w in self.orth_norm_basis:
+                    for a in self.scalar_factors:
+                        for b in self.scalar_factors:
                             self.assertVectorEquiv(
                                 cross(u, (v * a) + (w * b)),
                                 cross(u, v) * a + cross(u, w) * b,
                             )
+
+
+class InvertedTest(LinAlgTestCase):
+    def setUp(self) -> None:
+        v0 = AbstractVector(0.0, 0.0, 0.0)
+        e0 = AbstractVector(1.0, 0.0, 0.0)
+        e1 = AbstractVector(0.0, 1.0, 0.0)
+        e2 = AbstractVector(0.0, 0.0, 1.0)
+        orth_norm_basis = (e0, e1, e2)
+        self.m0 = AbstractMatrix(v0, v0, v0)
+        self.mI = AbstractMatrix(*orth_norm_basis)  # own inverse
+        self.m2 = AbstractMatrix(e0 * 1.0, e1 * 2.0, e2 * 3.0)
+        self.m2_inv = AbstractMatrix(e0 / 1.0, e1 / 2.0, e2 / 3.0)
+        self.m3 = AbstractMatrix(e0, e1, e0 * -1.0)  # linear dependency
+        self.m4 = AbstractMatrix(
+            AbstractVector(2, 3, 5),
+            AbstractVector(7, 11, 13),
+            AbstractVector(17, 19, 23),
+        )
+        self.m4_inv = (
+            AbstractMatrix(
+                AbstractVector(-6, -26, 16),
+                AbstractVector(-60, 39, -9),
+                AbstractVector(54, -13, -1),
+            )
+            / 78
+        )
+
+    def test_inverted(self) -> None:
+        """Tests matrix inversion."""
+        with self.assertRaises(ArithmeticError):
+            inverted(self.m0)
+        self.assertMatrixEquiv(inverted(self.mI), self.mI)
+        self.assertMatrixEquiv(inverted(self.m2), self.m2_inv)
+        with self.assertRaises(ArithmeticError):
+            inverted(self.m3)
+        self.assertMatrixEquiv(inverted(self.m4), self.m4_inv)
+
+
+class CoAndCoraviantTest(LinAlgTestCase):
+    # pylint: disable=R0902
+
+    def setUp(self) -> None:
+        e0 = AbstractVector(1.0, 0.0, 0.0)
+        e1 = AbstractVector(0.0, 1.0, 0.0)
+        e2 = AbstractVector(0.0, 0.0, 1.0)
+        orth_norm_basis = (e0, e1, e2)
+        self.v0 = AbstractVector(0.0, 0.0, 0.0)
+        self.v1 = AbstractVector(1.0, 2.0, 3.0)
+
+        mI = AbstractMatrix(*orth_norm_basis)
+        self.gI = Metric(mI)
+
+        m1 = AbstractMatrix(e0 * 2.0, e1 * 5.0, e2 * 7.0)
+        self.g1 = Metric(m1)
+        self.v11_co = AbstractVector(2.0, 10.0, 21.0)
+        self.v11_con = AbstractVector(1 / 2, 2 / 5, 3 / 7)
+        m2 = AbstractMatrix(
+            AbstractVector(2, 3, 5),
+            AbstractVector(7, 11, 13),
+            AbstractVector(17, 19, 23),
+        )
+        self.g2 = Metric(m2)
+        self.v12_co = AbstractVector(23, 68, 124)
+        self.v12_con = AbstractVector(-5 / 39, -3 / 26, 25 / 78)
+
+        self.gs = (self.gI, self.g1, self.g2)
+        self.vs = (self.v0, self.v1)
+
+    def test_co_variant(self) -> None:
+        """Tests covariant conversion."""
+        self.assertVectorEquiv(covariant(self.gI, self.v0), self.v0)
+        self.assertVectorEquiv(covariant(self.g1, self.v0), self.v0)
+        self.assertVectorEquiv(covariant(self.g1, self.v1), self.v11_co)
+        self.assertVectorEquiv(covariant(self.g2, self.v1), self.v12_co)
+
+    def test_contra_variant(self) -> None:
+        """Tests contravariant conversion."""
+        self.assertVectorEquiv(contravariant(self.gI, self.v0), self.v0)
+        self.assertVectorEquiv(contravariant(self.g1, self.v0), self.v0)
+        self.assertVectorEquiv(contravariant(self.g1, self.v1), self.v11_con)
+        self.assertVectorEquiv(contravariant(self.g2, self.v1), self.v12_con)
+
+    def test_co_contra_variant_invertibility(self) -> None:
+        """Tests if co- and contravariant invert each other."""
+        for g in self.gs:
+            for v in self.vs:
+                self.assertVectorEquiv(covariant(g, contravariant(g, v)), v)
+                self.assertVectorEquiv(contravariant(g, covariant(g, v)), v)
 
 
 if __name__ == "__main__":
