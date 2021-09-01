230cd1a355d39b089c345fe3241316d46a15dd74
Kwasniok <Kwasniok@users.noreply.github.com>
2021-09-01 06:12:33 +0200

    update coordinates

--- src/nerte/values/coordinates.py
+++ src/nerte/values/coordinates.py
@@ -4,3 +4,4 @@ from typing import NewType
 
 Coordinates3D = NewType("Coordinates3D", tuple[float, float, float])
 Coordinates2D = NewType("Coordinates2D", tuple[float, float])
+Coordinates1D = NewType("Coordinates1D", float)
