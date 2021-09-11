d0d6c6fc437f2af4c72482e0c723a543496cb1ac
Kwasniok <Kwasniok@users.noreply.github.com>
2021-09-11 17:31:02 +0200

    refactor (small optimization)

--- src/nerte/geometry/carthesian_geometry.py
+++ src/nerte/geometry/carthesian_geometry.py
@@ -1,7 +1,7 @@
 """Module for representing an euclidean geometry in carthesian coordinates."""
 
 from nerte.values.coordinates import Coordinates3D
-from nerte.values.linalg import AbstractVector, length
+from nerte.values.linalg import AbstractVector, normalized, is_zero_vector
 from nerte.values.face import Face
 from nerte.values.ray_segment import RaySegment
 from nerte.values.intersection_info import IntersectionInfo
@@ -17,13 +17,18 @@ class CarthesianGeometry(Geometry):
         """Represenation of a ray in euclidean geometry in Carthesian coordinates."""
 
         def __init__(self, start: Coordinates3D, direction: AbstractVector):
+            if is_zero_vector(direction):
+                raise ValueError(
+                    "Cannot construct carthesian ray with zero vector as direction."
+                )
+            direction = normalized(direction)
             self._segment = RaySegment(
                 start=start, direction=direction, is_finite=False
             )
 
         def intersection_info(self, face: Face) -> IntersectionInfo:
             ray_depth = intersection_ray_depth(ray=self._segment, face=face)
-            ray_depth *= length(self._segment.direction)
+            # no length factor required since segment is normalized
             return IntersectionInfo(ray_depth=ray_depth)
 
         def as_segment(self) -> RaySegment:
