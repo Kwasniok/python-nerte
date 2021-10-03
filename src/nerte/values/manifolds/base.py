ab3650c53c718b679e606d1d8a264a87de099b63
Kwasniok <Kwasniok@users.noreply.github.com>
2021-10-03 23:50:18 +0200

    remove manifolds module

--- src/nerte/values/manifolds/base.py
+++
@@ -1,8 +0,0 @@
-"""Base Module for representations of manifolds."""
-
-
-class OutOfDomainError(ValueError):
-    # pylint: disable=W0107
-    """Raised when a manifold parameter is outside of the domain."""
-
-    pass
--- src/nerte/values/manifolds/cylindrical/__init__.py
+++
@@ -1,8 +0,0 @@
-# TODO
-
-from .base import (
-    are_valid_coords,
-    invalid_coords_reason,
-    metric,
-    geodesic_equation,
-)
