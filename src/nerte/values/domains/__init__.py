d6b392570e1b844c4086172f2be551e39ae73ee6
Kwasniok <Kwasniok@users.noreply.github.com>
2021-10-08 17:02:52 +0200

    remove note #60

--- src/nerte/values/domains/__init__.py
+++ src/nerte/values/domains/__init__.py
@@ -4,3 +4,5 @@ from .base import OutOfDomainError
 from .domain_1d import Domain1D, R1, EMPTY1D, CartesianProduct1D
 from .domain_2d import Domain2D, R2, EMPTY2D, CartesianProduct2D
 from .domain_3d import Domain3D, R3, EMPTY3D, CartesianProduct3D
+
+from .characteristic_function_2d import CharacteristicFunction2D
