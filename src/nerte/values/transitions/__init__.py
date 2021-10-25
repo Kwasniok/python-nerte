c387a0eeb0cb100d8fd882ddc6809f4e4e1a8489
Kwasniok <Kwasniok@users.noreply.github.com>
2021-10-25 21:40:11 +0200

    remove chained transition #65

--- src/nerte/values/transitions/__init__.py
+++ src/nerte/values/transitions/__init__.py
@@ -4,5 +4,5 @@ from .transition_3d import (
     Transition3D,
     Identity as IdentityTransition,
 )
-from .chained_transition_3d import ChainedTransition3D
+from .inverse_transition_3d import InverseTransition3D
 from .linear_3d import Linear3D
